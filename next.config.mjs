6cf448874d825b0d2be5fcf890a18002fea7f63f
Mahamudul Hasan <mahamudul.47.bd@gmail.com>
2026-03-05 15:39:45 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    // বিল্ড করার সময় টাইপ এরর থাকলেও সে এগিয়ে যাবে
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // বিল্ডের সময় লিন্টিং এরর ইগনোর করবে
+    ignoreDuringBuilds: true,
+  },
 };
+
 export default nextConfig;
